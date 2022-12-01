e41baa0b5cdf919fcbd59170cdead5a3b1b3bdb8
Jason <loktar69@hotmail.com>
2022-12-01 01:03:40 -0500

    Day 1

---
+++ day 1/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+fs.readFile('data.txt', 'utf8' , (err, data) => {
+    const inputData = [0];
+    let inputIndex = 0;
+
+    [...data.replace(/[\r]/g, '').split('\n')].forEach((data) => {
+        if (data) {
+            inputData[inputIndex] += Number(data);
+        } else {
+            inputIndex++;
+            inputData[inputIndex] = 0;
+        }
+    });
+
+    const total = inputData.sort((a, b) => b - a).slice(0, 3).reduce((prev, cur) => prev + cur);
+
+    console.log(total);
+});
