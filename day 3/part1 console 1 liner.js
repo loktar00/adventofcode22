7a34e7e4f4cd5f92568d9678a85b3cbc98722499
Jason <loktar69@hotmail.com>
2022-12-03 01:00:39 -0500

    Added 1 liner in the console for day 3

---
+++ day 3/part1 console 1 liner.js
@@ -0,0 +1,3 @@
+// https://adventofcode.com/2022/day/3/input
+
+[...document.querySelector('pre').textContent.split(/\n/)].reduce((acc, cur) => {acc + Number(cur.substring(0, cur.length / 2).split('').filter(i => cur.substring(cur.length / 2).includes(i)).splice(0,1).map(i => {c=i.charCodeAt(0);(c < 97) ? c += -38 : c -= 96; return c;}));}, 0);
