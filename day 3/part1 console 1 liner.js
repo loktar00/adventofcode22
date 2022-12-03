ddba80cb23ddafb3c1495c0a6809cb32882be2f8
Jason <loktar69@hotmail.com>
2022-12-03 01:07:36 -0500

    Fixed issue with 1 liner

--- day 3/part1 console 1 liner.js
+++ day 3/part1 console 1 liner.js
@@ -1,3 +1,2 @@
 // https://adventofcode.com/2022/day/3/input
-
-[...document.querySelector('pre').textContent.split(/\n/)].reduce((acc, cur) => {acc + Number(cur.substring(0, cur.length / 2).split('').filter(i => cur.substring(cur.length / 2).includes(i)).splice(0,1).map(i => {c=i.charCodeAt(0);(c < 97) ? c += -38 : c -= 96; return c;}));}, 0);
+[...document.querySelector('pre').textContent.split(/\n/)].reduce((acc, cur) => {return acc + Number(cur.substring(0, cur.length / 2).split('').filter(i => cur.substring(cur.length / 2).includes(i)).splice(0,1).map(i => {c=i.charCodeAt(0);(c < 97) ? c += -38 : c -= 96; return c;}));}, 0);
