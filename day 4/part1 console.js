55b1a139d6f32e125080a1731b50e5929cc9f172
Jason <loktar69@hotmail.com>
2022-12-04 02:08:13 -0500

    improved 1 line

--- day 4/part1 console.js
+++ day 4/part1 console.js
@@ -1,10 +1,2 @@
-//https://adventofcode.com/2022/day/4/input
-[...document.querySelector('pre').textContent.split(/\n/)]
-    .filter(group => group && group.split(',')
-    .map(i => i.split('-')
-    .map(i => Number(i))
-    .some((el, _, arr) => {
-        console.log(arr[1][0], el[0])
-        return (arr[1][0] <= el[0] && arr[1][1] >= el[1]) || (el[0] <= arr[1][0] && el[1] >= arr[1][1])
-    })
-)).length;
+    //https://adventofcode.com/2022/day/4/input
+    [...document.querySelector('pre').textContent.split(/\n/)].filter(group => group && group.split(',').map(i => i.split('-').map(i => Number(i))).every((el, _, arr) =>  (arr[1][0] <= el[0] && arr[1][1] >= el[1]) || (el[0] <= arr[1][0] && el[1] >= arr[1][1]))).length;
